9abcea062066a1c3941d8712d17ae65162e529b9
kasambuli <cynthiakasambuli@gmail.com>
2019-08-21 12:23:22 +0300

    Added a form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,20 +9,30 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
         get("/", (request, response) ->{
-//
             Map<String,Object> model = new HashMap<String, Object>();//new model to store information
                     return new ModelAndView(model,"hello.hbs");//assemble individual pieces and render
-                },
-                new HandlebarsTemplateEngine()
+                }, new HandlebarsTemplateEngine());
 
-        );
 
         get("/favorite_photos", (request, response) ->{
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "favorite_photos.hbs");
-                }, new HandlebarsTemplateEngine()
+                }, new HandlebarsTemplateEngine());
 
-        );
 
+        get ("/form", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model,"form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+         get("/greeting_card",(request, response) -> {
+             Map<String, Object> model = new HashMap<String, Object>();
+             String recipient = request.queryParams("recipient");
+             String sender = request.queryParams("sender");
+             model.put("recipient",recipient);
+             model.put("sender",sender);
+             return new ModelAndView(model,"greeting_card.hbs");
+         }, new HandlebarsTemplateEngine());
     }
 }
